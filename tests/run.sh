dc35518e4f7fc6d6de415742d5d9b7b521a17c31
Colin Jermain <cjermain@gmail.com>
2020-01-22 20:26:24 -0500

    Adding exit codes to catch failures

--- tests/run.sh
+++ tests/run.sh
@@ -7,9 +7,9 @@ ERR=`diff -u test.exp test.out`
 if [ -n "$ERR" ] ; then
 	echo "$ERR" > test.err
 	echo "Tests failed - see test.err"
+    exit 1
 else
 	rm -f test.err
 	echo "Tests passed"
+    exit 0
 fi
-
-
