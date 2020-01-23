1f3e52cfef5c1bb8ab2902e70e614717689d446c
Colin Jermain <cjermain@gmail.com>
2020-01-22 21:09:53 -0500

    Adding default Collection setup

---
+++ tests/set_default_collection.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+import gi
+gi.require_version('Secret', '1')
+from gi.repository import Secret
+
+service = Secret.Service.get_sync(Secret.ServiceFlags.LOAD_COLLECTIONS)
+
+collection = Secret.Collection.for_alias_sync(service, Secret.COLLECTION_DEFAULT, Secret.CollectionFlags.NONE)
+
+if collection is None:
+    Secret.Collection.create_sync(Secret.COLLECTION_DEFAULT, Secret.CollectionFlags.NONE)
+    print("Created Secret.Collection {}".format(collection.get_label())
+
