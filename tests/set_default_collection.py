8fde132b9b9ee26efebca5d5c8158c0f09087c6f
Colin Jermain <cjermain@gmail.com>
2020-01-22 21:11:24 -0500

    Fixing syntax error in Python script

--- tests/set_default_collection.py
+++ tests/set_default_collection.py
@@ -10,5 +10,5 @@ collection = Secret.Collection.for_alias_sync(service, Secret.COLLECTION_DEFAULT
 
 if collection is None:
     Secret.Collection.create_sync(Secret.COLLECTION_DEFAULT, Secret.CollectionFlags.NONE)
-    print("Created Secret.Collection {}".format(collection.get_label())
+    print("Created Secret.Collection {}".format(collection.get_label()))
 
